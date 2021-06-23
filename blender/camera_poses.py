80f4f7bbe6c26222304b210d76229b391254e3d6
Markus Suchi <suchi@acin.tuwien.ac.at>
2021-06-23 13:41:07 +0200

    Blender refacotring.

---
+++ blender/camera_poses.py
@@ -0,0 +1,99 @@
+import mathutils
+import math
+import numpy as np
+import bpy
+import glob
+
+def get_poses(path_groundtruth, invert=False):                                
+    with open(path_groundtruth) as fp:                          
+        groundtruth = fp.readlines()                                
+        groundtruth = [line.strip().split() for line in groundtruth]
+                                                                
+    trans = [line[1:4] for line in groundtruth]                 
+    quat = [line[4:] for line in groundtruth]                   
+                                                                
+    poses = []                                              
+    for i in range(len(groundtruth)):                           
+        pose = np.zeros((4, 4))                                 
+        pose[3, 3] = 1      
+        pose_quat_np = np.array([quat[i][3],quat[i][0],quat[i][1],quat[i][2]], dtype=np.float32)
+        pose_quat = mathutils.Quaternion(pose_quat_np).to_matrix()  
+        print(pose_quat) 
+        pose[:3, :3] = pose_quat
+        pose[:3, -1] = trans[i]    
+        if invert:  
+            pose = np.linalg.inv(pose) 
+        poses.append(pose)                                                                                                      
+    return poses       
+
+def get_blender_camera(K, width, height, sensor_width_in_mm):
+    w = width
+    h = height
+    lens = K[0] *  sensor_width_in_mm / w
+    shift_x = -(K[0,2] / w - 0.5)
+    shift_y = (K[1,2] - 0.5 * h) / w
+    return lens, shift_x, shift_y          
+
+def add_cameras(camera_path = "", camera_rgb_path = "", invert=False): 
+    camera_poses = get_poses(camera_path, invert)
+    
+    groundtruth_to_blender = np.array([[1, 0, 0, 0],
+                                       [0, -1, 0, 0],
+                                       [0, 0, -1, 0],
+                                       [0, 0, 0, 1]])
+
+    # remove old cameras    
+    if "cameras" in bpy.data.collections:
+        for o in bpy.data.collections["cameras"].objects:
+            print(o.name)
+            if "Camera_" in o.name:
+                bpy.data.objects.remove(o, do_unlink=True)
+    rgb = []
+    rgb = glob.glob(camera_rgb_path + "*.png")
+    rgb.sort()
+    
+    K = np.asarray([[923.101, 0 , 629.313476], 
+                    [0, 922.568, 376.28814687], 
+                    [0, 0, 1]])                     
+    sensor_width_in_mm = 2.7288
+    width = 1280
+    height = 720
+
+    for i in range(len(camera_poses)):
+      name = "Camera_" + str(i)
+      
+      if "cameras" not in bpy.data.collections:
+          cam_collection = bpy.ops.collection.create(name  = "cameras")
+          bpy.context.scene.collection.children.link(bpy.data.collections["cameras"])
+
+      if bpy.data.collections["cameras"].objects.get(name):
+          assert(False, "Tried to overwrite camera.")
+      else:      
+          cam1 = bpy.data.cameras.new(name)
+          obj_camera = bpy.data.objects.new(name, cam1)
+          camera_poses[i] = camera_poses[i].dot(groundtruth_to_blender)
+          location = camera_poses[i][:3,-1]
+          rotation = camera_poses[i][:3, :3]
+          obj_camera.location = [location[0], location[1], location[2]]
+          obj_camera.rotation_euler = mathutils.Matrix(rotation).to_euler()
+        
+          lens, shift_x, shift_y = get_blender_camera(K, width, height, sensor_width_in_mm)
+          obj_camera.data.lens_unit = 'MILLIMETERS'
+          obj_camera.data.lens = lens
+          obj_camera.data.shift_x = shift_x
+          obj_camera.data.shift_y = shift_y
+          obj_camera.data.display_size = 0.05
+          obj_camera.data.sensor_width = sensor_width_in_mm
+          
+          # set background image
+          img = bpy.data.images.load(rgb[i])
+          obj_camera.data.show_background_images = True
+          bg = obj_camera.data.background_images.new()
+          bg.image = img
+          bg.alpha = 1.0
+          bpy.data.collections["cameras"].objects.link(obj_camera)
+
+camera_path = "/home/markus/temp/TestDataset/scenes/006/groundtruth_handeye.txt" 
+scene_path = "/home/markus/temp/TestDataset/scenes/006/rgb/"
+
+add_cameras(camera_path = camera_path, camera_rgb_path = scene_path)
---
+++ blender/save_pannel.py
@@ -0,0 +1,60 @@
+import os,sys
+import bpy
+import numpy as np
+from numpy.linalg import inv
+import yaml
+
+class save_pose(bpy.types.Operator):
+    bl_idname="object.save"
+    bl_label = "Save Pose"
+    filepath: bpy.props.StringProperty(subtype="FILE_PATH", default = "./poses.yaml")
+    
+    def execute(self,context):
+        print("Saving poses to: " + self.filepath)
+        output_list = []
+        for obj in bpy.data.collections['objects'].objects:
+                print("obj: ", obj.name)
+                pose = np.zeros((4,4))
+                pose[:,:]=obj.matrix_world    
+                #try to get to same coordinate system as jnb reprojection
+                #pose[:3,:3] = pose[:3, :3] * -1
+                #pose[:3, 0] = pose[:3, 0] * -1 
+                pose = pose.reshape(-1)
+                output_list.append({"path":"-/Test.ply","id":obj.name,"pose":pose.tolist()})
+
+        if output_list:       
+            print(yaml.dump(output_list, default_flow_style=False))
+            with open(self.filepath,'w') as f:
+                yaml.dump(output_list, f, default_flow_style=False)
+      
+        return {'FINISHED'}
+    
+    def invoke(self,context, event):
+        #set filepath with default value of property
+        self.filepath = self.filepath
+        context.window_manager.fileselect_add(self)
+        print(self.filepath)
+        return {'RUNNING_MODAL'}
+       
+class PoseAnnotationPanel(bpy.types.Panel):
+    bl_label = "Pose Annotation"
+    bl_idname = "3D_VIEW__PT_bricks_1"
+    bl_space_type = "VIEW_3D"
+    bl_region_type = "TOOLS"
+
+    def draw(self, context):
+        layout = self.layout
+        row=layout.row()
+        row.alignment = 'CENTER'
+        row.operator("object.save")
+        
+def register():
+    bpy.utils.register_class(save_pose)
+    bpy.utils.register_class(PoseAnnotationPanel)
+    
+def unregister():
+    bpy.utils.unregister_class(save_pose)
+    bpy.utils.unregister_class(PoseAnnotationPanel)
+
+if __name__ == "__main__":
+    register()
