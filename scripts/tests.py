2bd24a8a572a88244a7e0c8f56c9b1c753fbed25
Markus Suchi <briocherl@gmail.comt>
2021-07-20 15:30:14 +0200

    Scene handling and camera infos.

--- scripts/tests.py
+++ scripts/tests.py
@@ -15,52 +15,58 @@ def main():
     args = parser.parse_args()
 
     # Test Scene File Loader
+    print("--- SceneFileReader.")
     cfg = configparser.ConfigParser()
     cfg.read(args.config)
     reader = v4r.io.SceneFileReader(cfg['General'])
-    object_lib = v4r.objects.ObjectLibrary.create(reader.object_library_file)
-
-    # testing SceneFileReader
-    print("--- SceneFileReader.")
+    object_lib = reader.readObjectLibrary()
     print(reader)
+
     # Test MeshReader
     print("--- MeshReader")
     mesh_reader = v4r.io.MeshReader('/temp/dummy.ply')
     print(mesh_reader)
+
     # Test Object Library
     # single object access
     print("--- ObjectLibrary")
-    print("--- Single access.")
+    print("----- Single access.")
     obj = object_lib[1]
     print(obj)
     # list object access
-    print("--- Multi access with indices.")
+    print("----- Multi access with indices.")
     for obj in object_lib.as_list([1, 2]):
         print(obj.id, obj.name, obj.mesh.file)
-    print("--- Multi access without indices.")
+    print("----- Multi access without indices.")
     for obj in object_lib.as_list():
         print(obj.id, obj.name, obj.mesh.file)
-    print("--- Filter by class.")
+    print("----- Filter by class.")
     # filter by class
     for obj in [val for val in object_lib.values() if val.class_id in ['bottle']]:
         print(obj.id, obj.name, obj.class_id)
+
     # Camera intrinsics
-    print("--- CameraIntrinsics")
-    cam = v4r.io.CameraIntrinsic(1, 2, 3, 4, 5, 6, 100)
+    print("--- CameraInfo")
+    cam = v4r.io.CameraInfo('testcam', 1, 2, 3, 4, 5, 6)
     print(cam)
     print(cam.as_numpy3x3())
     print(np.shape(cam.as_numpy3x3()))
     print(cam.sensor_width_mm)
-    print("--- CameraIntrinsics from file")
+    print("--- CameraInfo from reader")
     cam = reader.readCameraInfo()
     print(cam)
     print(cam.as_numpy3x3())
+
     # Pose
     print("--- Pose")
     p1 = v4r.io.Pose(np.array(np.eye(4)))
     p2 = v4r.io.Pose([0, 0, 0, 1, 0, 0, 0])
     p3 = v4r.io.Pose()
     print(f'p1:\n{p1}\np2:\n{p2}\np3:\n{p3}\n')
+
+    # SceneIds
+    print("--- Scene Ids")
+    print(reader.getSceneIds())
 
 
 if __name__ == "__main__":
--- v4r_dataset_toolkit/io.py
+++ v4r_dataset_toolkit/io.py
@@ -1,4 +1,3 @@
-import glob
 import open3d as o3d
 import os
 import numpy as np
@@ -6,18 +5,6 @@ import yaml
 
 from .objects import ObjectLibrary
 from .meshreader import MeshReader
-
-
-class Scene:
-    def __init__(self, scene_id=None, rgb=None, depth=None, cameras=None, objects=None, markers=None, mesh_file=None):
-        self.scene_id = scene_id
-        self.rgb_files = rgb_files or []  # o3d image
-        self.depth_files = depth_files or []  # o3d image
-        self.cameras = cameras or []  # o3d camera trajectory
-        # TODO: objects/object id with numpy 4x4 array (saved as quaternion)
-        self.objects = objects or []
-        self.markers = markers or []  # numpy 4x4 array (saved as quaternion)
-        self.mesh = MeshReader(mesh_file)
 
 
 # poses from groundtruth text file, each line
@@ -41,6 +28,7 @@ class Pose:
         else:
             raise ValueError("Pose unsupported type.")
 
+    # from list tx,ty,tz,rw,rx,ry,rz
     def set_list(self, tf):
         if(len(tf) == 7):
             self.tf = np.zeros((4, 4))
@@ -59,6 +47,8 @@ class Pose:
 
     def __str__(self):
         return self.tf.__str__()
+
+# Need to get pose array for camera(s)
 
 
 def get_poses(path_groundtruth):
@@ -81,11 +71,14 @@ def get_poses(path_groundtruth):
 
     return poses
 
+# Need to get pose for annotated objects
 
-class CameraIntrinsic:
-    def __init__(self, width=None, height=None,
+
+class CameraInfo:
+    def __init__(self, name=None, width=None, height=None,
                  fx=None, fy=None, cx=None, cy=None,
                  sensor_width_mm=None):
+        self.name = name
         self.width = width
         self.height = height
         self.fx = fx
@@ -95,7 +88,8 @@ class CameraIntrinsic:
         self.sensor_width_mm = sensor_width_mm
 
     def __str__(self):
-        return f'width : {self.width}\n' \
+        return f'name: {self.name}\n' \
+            f'width : {self.width}\n' \
             f'height: {self.height}\n' \
             f'fx: {self.fx}\n' \
             f'fy: {self.fy}\n' \
@@ -110,14 +104,19 @@ class CameraIntrinsic:
         return np.array([[self.fx, 0, self.cx], [0, self.fy, self.cy], [0, 0, 1]])
 
     def as_blender(self):
+        # TODO: Either create the blender camera here and import bpy
+        #       or have a blender class which does it and takes this here as input
+        #       or call the blender class here and return it
+        #       Needs to check if blender is actual available
         pass
 
     @classmethod
     def create(cls, file):
-        # Read widht, height and intrinsics from yaml file
+        # Read width, height and intrinsics from yaml file
         with open(file, 'r') as fp:
             cam = yaml.load(fp, Loader=yaml.FullLoader)
-            return cls(cam.get('image_width'),
+            return cls(cam.get('name'),
+                       cam.get('image_width'),
                        cam.get('image_height'),
                        cam.get('camera_matrix')[0],
                        cam.get('camera_matrix')[4],
@@ -128,15 +127,27 @@ class CameraIntrinsic:
 
 
 class CameraTrajectory:
-    def __init__(self, camera_intrinsics, poses=[]):
-        self.camera_intrinsics = camera_intrinsics
+    def __init__(self, camera_info, poses=[]):
+        self.camera_info = camera_info
         self.poses = poses
 
     @classmethod
     def create(cls, camera_file, pose_file):
         # Read camera params
-        self.camera_intrinsics = CameraIntrinsic.create(camera_file)
+        self.camera_info = CameraInfo.create(camera_file)
         # Read poses saved as quaternions and save as np.array 3x3
+
+
+class Scene:
+    def __init__(self, scene_id=None, rgb=None, depth=None, cameras=None, objects=None, markers=None, reconstruction_file=None):
+        self.scene_id = scene_id
+        self.rgb_files = rgb_files or []  # o3d image
+        self.depth_files = depth_files or []  # o3d image
+        self.cameras = cameras or []  # o3d camera trajectory
+        # TODO: objects/object id with numpy 4x4 array (saved as quaternion)
+        self.objects = objects or []
+        self.markers = markers or []  # numpy 4x4 array (saved as quaternion)
+        self.reconstruction = MeshReader(mesh_file)
 
 
 class SceneFileReader:
@@ -147,10 +158,12 @@ class SceneFileReader:
         self.depth_dir = config.get('depth_dir')
         self.camera_pose_file = config.get('camera_pose_file')
         self.camera_intrinsics_file = config.get('camera_intrinsics_file')
-        self.object_pose_file = config.get('object_pose_file')
         self.object_library_file = config.get('object_library_file')
         self.associations_file = config.get('associations_file')
-        self.reconstruction_dir = config.get('reconstruction_dir')
+        # How to separate recordings from annotations?
+        self.object_pose_file = config.get('object_pose_file')
+        self.reconstruction_file = config.get('reconstruction_file')
+        self.mask_dir = config.get('mask_dir')
 
     def __str__(self):
         return f'root_dir: {self.root_dir}\n'\
@@ -159,20 +172,27 @@ class SceneFileReader:
             f'depth_dir: {self.depth_dir}\n'\
             f'camera_pose_file: {self.camera_pose_file}\n'\
             f'camera_intrinsics_file: {self.camera_intrinsics_file}\n'\
-            f'object_pose_file: {self.object_pose_file}\n'\
             f'object_library_file: {self.object_library_file}\n'\
             f'associations_file: {self.associations_file}\n'\
-            f'reconstruction_dir: {self.reconstruction_dir}'
-
-    def readScenes(self):
-        pass
+            f'object_pose_file: {self.object_pose_file}\n'\
+            f'reconstruction_file: {self.reconstruction_file}\n'\
+            f'mask_dir: {self.mask_dir}'
 
     def readCameraInfo(self):
-        full_path = os.path.join(self.scenes_dir, self.camera_intrinsics_file)
-        return CameraIntrinsic.create(full_path)
+        full_path = os.path.join(
+            self.root_dir, self.scenes_dir, self.camera_intrinsics_file)
+        return CameraInfo.create(full_path)
 
     def readObjectLibrary(self):
-        pass
+        return ObjectLibrary.create(self.object_library_file)
 
-    def readPose(self):
+    def getSceneIds(self):
+        full_path = os.path.join(self.root_dir, self.scenes_dir)
+        return sorted([f.name for f in os.scandir(full_path) if f.is_dir()])
+
+    def readScene(self, id):
+        # check if id is in this datasets scene id list
+
+        # access to specific scene should be a function
+        # access to specific rgbs/depths should be loaded on demand
         pass
