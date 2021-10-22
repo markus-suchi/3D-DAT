8ab24af0beb0735cc4abd36f2f608dbbd0cae20c
Markus Suchi <briocherl@gmail.comt>
2021-10-22 12:58:42 +0200

    Deleted obsolete output messages.

--- __init__.py
+++ __init__.py
@@ -11,23 +11,17 @@ bl_info = {
 
 if "bpy" in locals():
     import importlib
-    print("bpy local import")
     importlib.reload(v4r_annotation_tool)
     importlib.reload(v4r_dataset_toolkit)
     importlib.reload(cycle_cameras)
 else:
-    print("nono bpy import")
-    print("import v4r_dataset_toolkit")
     from . import v4r_dataset_toolkit as v4r
-    print("import blender v4r annotaiton tool")
     from .blender import v4r_annotation_tool
-    print("import blender cycle_cameras")
     from .blender import cycle_cameras
 
 
 #### REGISTER ###
 def register():
-    print("register")
     v4r_annotation_tool.register()
     cycle_cameras.register()
 
