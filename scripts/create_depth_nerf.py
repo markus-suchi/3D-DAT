110c4f4d82aca79d3e454c5a68f6d0f81a58c54d
Markus Suchi <briocherl@gmail.comt>
2023-05-05 14:59:48 +0200

    Commandline interface for multiple scenes.

--- scripts/create_depth_nerf.py
+++ scripts/create_depth_nerf.py
@@ -9,17 +9,10 @@ import main as nerf
 import yaml
 
 CONFIG = {
-    "debug_mode": True,
-    "max_depth": 1.3,
-    "voxel_size": 0.004,
-    "tsdf_cubic_size": 1.5,
-    "icp_method": "color",
-    "icp_refinement": False,
-    "save_refined": True,
-    "sdf_trunc": 0.018,
-    "triangles": 1000000,
-    "simplify": False,
-    "cluster" : False
+    "sigma_threshold": 9,
+    "aabb_scale": 4,
+    "train_steps": 5000,
+    "view": 360
 }
 
 def load_config(file):
@@ -31,51 +24,84 @@ if __name__ == "__main__":
     parser = argparse.ArgumentParser("Integrate the whole RGBD sequence using estimated camera pose.")
     parser.add_argument("-d", "--dataset", type=str, default="./config.cfg",
                         help="Path to data to reconstruct")
-    parser.add_argument("--scene_id", type=str, default=None,
+    parser.add_argument("--scene_id", nargs='*', type=str, default=None,
                         help="Scene identifier.")
+    parser.add_argument("--sigma_threshold", type=int, default=None,
+                        help="Sigma Threshold.")
+    parser.add_argument("--aabb_scale", type=int, default=None,
+                        help="Scale for crop_box.")
+    parser.add_argument("--train_steps", type=int, default=None,
+                        help="Training steps.")
+    parser.add_argument("--view", type=int, default=None,
+                        help="Camera views: 180 or 360 degrees.")
     args = parser.parse_args()
 
 
     config = None
     if(os.path.exists(args.dataset)):
         config = load_config(args.dataset)
-        #if not "Reconstruction" in config:
-        #    print(f"Configuration file {args.dataset} does not contain reconstruction parameter.")
-        #    print("Using default parameter.")
-        #    config=CONFIG
-        #else:
-        #    config=config.get('Reconstruction')
+        if not "Nerf" in config:
+            print(f"Configuration file {args.dataset} does not contain Nerf parameter.")
+            print("Using default parameter.")
+            config=CONFIG
+        else:
+            config=config.get('Nerf')
     else:
         print(f"The config {args.dataset} file does not exist.")
         os.sys.exit(1)
 
-    #if args.triangles:
-    #    config["triangles"]= args.triangles
+    if args.sigma_threshold:
+        config["sigma_threshold"]=args.sigma_threshold
 
-    #if args.icp_refinement:
-    #    config["icp_refinement"]=True
-    #    config["icp_method"]=args.icp_method
+    if args.aabb_scale:
+        config["aabb_scale"]=args.aabb_scale
 
-    #if args.simplify:
-    #    config["simplify"]=True
+    if args.train_steps:
+        config["train_steps"]=args.train_steps
 
-    #if args.cluster:
-    #    config["cluster"]=True
+    if args.view:
+        config["view"]=args.view
 
-    #print("Configuration:")
-    #for k, v in config.items():
-    #    print("\t", k, ":", v)
+    if config["view"] not in [180, 360]:
+        view_value = config["view"]
+        print(f"Error: The config paramter view for degrees of camera views {view_value} is invalid. Only 180 or 360 allowed.")
+        os.sys.exit(1)
+
+
+    print("Configuration:")
+    for k, v in config.items():
+        print("\t", k, ":", v)
 
     scene_file_reader = v4r.io.SceneFileReader.create(args.dataset)
+
+    # check if we have a scene_id parameter
+    scenes = []
+    all_scenes = scene_file_reader.get_scene_ids()
+    if args.scene_id:
+        scenes = sorted(args.scene_id)
+        # check if all scene ids are present
+        diff = [x for x in scenes if x not in all_scenes]
+        if diff:
+            print("Error: The following scenes are not part of the dataset:")
+            print(diff)
+            os.sys.exit(1)
+    else:
+        scenes = all_scenes
+
     print("Creating depth images from NeRF")
+    for scene_id in scenes:
+        print(f"Processing scene: {scene_id}")
+        path_groundtruth = scene_file_reader.get_camera_info_scene_path(scene_id) 
+        scene_dir = os.path.join(scene_file_reader.root_dir, scene_file_reader.scenes_dir, scene_id)
+        intrinsic = scene_file_reader.get_camera_info_scene(scene_id)
+        nerf_intrinsic_file = os.path.join(scene_dir,"intrinsics.txt")
+        with open(nerf_intrinsic_file,'w') as fp:
+           fp.write("%d %d %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %.12f %d\n" % (
+               intrinsic.width, intrinsic.height, intrinsic.fx, 0.0, intrinsic.cx, 0.0, intrinsic.fy, intrinsic.cy, 0.0, 0.0, 1.0, config["view"]
+           ))
+           fp.flush()
 
-    path_groundtruth = scene_file_reader.get_camera_info_scene_path(args.scene_id) 
-    scene_dirs = [os.path.join(scene_file_reader.root_dir, scene_file_reader.scenes_dir, args.scene_id)]
-
-    #create intrinsic config file
-    intrinsic = scene_file_reader.get_camera_info_scene(args.scene_id).as_o3d()
-        
-    #create depth images for current scene
-    nerf.create_depthmaps(scene_dirs=scene_dirs, img_dir=scene_file_reader.rgb_dir, depth_dir=scene_file_reader.depth_dir, sigma_thrsh=9, aabb_scale=4, train_steps=4000)
+        nerf.create_depthmaps(scene_dirs=[scene_dir], img_dir=scene_file_reader.rgb_dir, depth_dir=scene_file_reader.depth_dir, sigma_thrsh=config["sigma_threshold"], 
+                              aabb_scale=config["aabb_scale"], train_steps=config["train_steps"])
 
     print("Finished")
--- scripts/reconstruct.py
+++ scripts/reconstruct.py
@@ -5,7 +5,7 @@ import v4r_dataset_toolkit as v4r
 import yaml
 
 CONFIG = {
-    "debug_mode": True,
+    "debug_mode": False,
     "max_depth": 1.3,
     "voxel_size": 0.004,
     "tsdf_cubic_size": 1.5,
@@ -32,12 +32,22 @@ if __name__ == "__main__":
     parser.add_argument("--triangles", type=int, default=None,
                         help="Target triangles for simpilfication.")
     parser.add_argument("--icp_method", type=str, default="color",
-                        help="Target triangles for simpilfication.")
+                        help="Icp-method one of ['point_to_point','robust_icp','point_to_plane','color'].")
     parser.add_argument("--simplify", action="store_true",
                         help="Do simplify reconstruction.")
     parser.add_argument("--cluster", action="store_true",
                         help="Use largest triangle cluster only.")
-    parser.add_argument("--scene_id", type=str, default=None,
+    parser.add_argument("--debug_mode", action="store_true",
+                        help="Visualize reconstruction.")
+    parser.add_argument("--max_depth", type=float, default=None,
+                        help="Set max depth in meter.")
+    parser.add_argument("--voxel_size", type=float, default=None,
+                        help="Set voxel size in meter.")
+    parser.add_argument("--tsdf_cubic_size", type=float, default=None,
+                        help="Set size of tsdf cube in meter.")
+    parser.add_argument("--sdf_trunc", type=float, default=None,
+                        help="Truncation value for signed distance function.")
+    parser.add_argument("--scene_id", nargs='*', type=str, default=None,
                         help="Scene identifier.")
     args = parser.parse_args()
 
@@ -68,30 +78,61 @@ if __name__ == "__main__":
     if args.cluster:
         config["cluster"]=True
 
+    if args.debug_mode:
+        config["debug_mode"]= args.debug_mode
+
+    if args.max_depth:
+        config["max_depth"]= args.max_depth
+
+    if args.voxel_size:
+        config["voxel_size"]= args.voxel_size
+
+    if args.tsdf_cubic_size:
+        config["tsdf_cubic_size"]= args.tsdf_cubic_size
+
+    if args.sdf_trunc:
+        config["sdf_trunc"]= args.sdf_trunc
+
     print("Configuration:")
     for k, v in config.items():
         print("\t", k, ":", v)
 
     scene_file_reader = v4r.io.SceneFileReader.create(args.dataset)
-    print("Creating Reconstructor")
-    # Refine with ICP
-    path_groundtruth = scene_file_reader.get_camera_info_scene_path(args.scene_id) 
-    path_dataset = os.path.join(
-        scene_file_reader.reconstruction_dir,
-        args.scene_id)
 
-    color_files = scene_file_reader.get_images_rgb(args.scene_id)
-    depth_files = scene_file_reader.get_images_depth(args.scene_id)
-    intrinsic = scene_file_reader.get_camera_info_scene(args.scene_id).as_o3d()
-    poses = scene_file_reader.get_camera_poses(args.scene_id)
+# check if we have a scene_id parameter
+    scenes = []
+    all_scenes = scene_file_reader.get_scene_ids()
+    if args.scene_id:
+        scenes = sorted(args.scene_id)
+        # check if all scene ids are present
+        diff = [x for x in scenes if x not in all_scenes]
+        if diff:
+            print("Error: The following scenes are not part of the dataset:")
+            print(diff)
+            os.sys.exit(1)
+    else:
+        scenes = all_scenes
 
-    reconstructor = v4r.reconstructor.Reconstructor(config=config, 
-                                  color_files = color_files,
-                                  depth_files = depth_files,
-                                  intrinsic = intrinsic,
-                                  poses = poses,
-                                  path_groundtruth = path_groundtruth,
-                                  path_dataset = path_dataset)
+    print("Creating Reconstruction")
+    for scene_id in scenes:
+        print(f"Processing scene: {scene_id}")
+        path_groundtruth = scene_file_reader.get_camera_info_scene_path(scene_id) 
+        path_dataset = os.path.join(
+            scene_file_reader.reconstruction_dir,
+            scene_id)
 
-    reconstructor.create_reconstruction()
+        color_files = scene_file_reader.get_images_rgb(scene_id)
+        depth_files = scene_file_reader.get_images_depth(scene_id)
+        intrinsic = scene_file_reader.get_camera_info_scene(scene_id).as_o3d()
+        poses = scene_file_reader.get_camera_poses(scene_id)
+
+        reconstructor = v4r.reconstructor.Reconstructor(config=config, 
+                                      color_files = color_files,
+                                      depth_files = depth_files,
+                                      intrinsic = intrinsic,
+                                      poses = poses,
+                                      path_groundtruth = path_groundtruth,
+                                      path_dataset = path_dataset)
+
+        reconstructor.create_reconstruction()
     print("Finished")
